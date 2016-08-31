f4bf479023b8d060cb49d8afd9dc00d88e214977
guilk <guiliangke@gmail.com>
2016-08-30 22:52:32 -0700

    run vgg face again

--- deeplearning/evaluation/extract_bosphorus.m
+++ deeplearning/evaluation/extract_bosphorus.m
@@ -5,11 +5,11 @@ model_root = '/usr0/home/liangkeg/InMind/FG/models/caffe/models/';
 % model_def_file = 'bvlc_alexnet/deploy.prototxt';
 % model_file = 'bvlc_alexnet/bvlc_alexnet.caffemodel';
 
-% model_def_file = 'vgg_face_caffe/VGG_FACE_deploy.prototxt';
-% model_file = 'vgg_face_caffe/VGG_FACE.caffemodel';
+model_def_file = 'vgg_face_caffe/VGG_FACE_deploy.prototxt';
+model_file = 'vgg_face_caffe/VGG_FACE.caffemodel';
 
-model_def_file = 'vgg/VGG_ILSVRC_16_layers_deploy.prototxt';
-model_file = 'vgg/VGG_ILSVRC_16_layers.caffemodel';
+% model_def_file = 'vgg/VGG_ILSVRC_16_layers_deploy.prototxt';
+% model_file = 'vgg/VGG_ILSVRC_16_layers.caffemodel';
 
 def_path = fullfile(model_root,model_def_file);
 model_path = fullfile(model_root, model_file);
@@ -37,7 +37,7 @@ for i = 1:numel(folders)
     for j = 1:numel(images)
         im_list{end+1} = fullfile(folder_path,images(j).name);
     end
-    extract_features_vgg(net, im_list, dst_folder_path);
+    extract_features_vgg_face(net, im_list, dst_folder_path);
 end
 
 caffe.reset_all();
